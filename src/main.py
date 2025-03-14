800ce602c3e1fd1ad9799072ab4f2aa68deacaae
light <caiocr610@gmail.com>
2025-03-14 16:53:15 -0300

    first commit
    gitignore, adicionando o arquivo csv e uma main que lê o conteúdo do arquivo csv

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import csv
+
+def main():
+    with open('rym_clean1.csv', 'r') as file:
+        reader = csv.reader(file)  # reader object, pode ser convertido em uma lista (de listas)
+        # cada row é uma lista
+        for row in reader:
+            print(row)
+
+if __name__ == '__main__':
+    main()
