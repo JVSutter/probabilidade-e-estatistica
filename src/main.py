04a30e85d8ef4a6d29d2ab4c056110a2aa63dfa9
light <caiocr610@gmail.com>
2025-03-14 17:16:52 -0300

    criando pasta assets

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import csv
 
 def main():
-    with open('rym_clean1.csv', 'r') as file:
+    with open('../assets/rym_clean1.csv', 'r') as file:
         reader = csv.reader(file)  # reader object, pode ser convertido em uma lista (de listas)
         # cada row é uma lista
         for row in reader:
